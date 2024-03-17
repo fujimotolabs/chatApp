2e9d67cee25f7065b46c3109b526fea1a0ef0350
ei-sugimoto <eikun1014@gmail.com>
2024-03-17 18:32:11 +0900

    database接続およびusersテーブルの作成＋seed作成

--- api/src/db/seeds.rb
+++ api/src/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create a user
+user = User.create(
+    name: "Test",
+    email: "test@gmail.com",
+    password: "password",
+)
