00c98e985e9fc64978e8635bba1102fbf4362899
ei-sugimoto <eikun1014@gmail.com>
2024-04-10 16:42:41 +0900

    メッセージ検索を行うAPI追加

--- api/src/app/controllers/api/messages_controller.rb
+++ api/src/app/controllers/api/messages_controller.rb
@@ -12,5 +12,17 @@ module Api
         render json: { status: 'ERROR', message: 'Message not saved', data: message.errors }, status: :unprocessable_entity
       end
     end
+
+    def show
+      user_id = params[:id]
+      user = User.find(user_id)
+      messages = user.messages
+      render json: { status: 'SUCCESS', message: 'Loaded messages', data: messages }, status: :ok
+    end
+    
+    def index
+      messages = Message.all
+      render json: { status: 'SUCCESS', message: 'Loaded messages', data: messages }, status: :ok
+    end
   end
 end
--- api/src/config/routes.rb
+++ api/src/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :users, only: [:index]
-    resources :messages, only: [:create, :show]
+    resources :messages, only: [:create, :show, :index]
   end
 end
