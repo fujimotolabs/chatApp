2d78bbfa0dc283bb4011a4b68f54654444a9b6ca
ei-sugimoto <eikun1014@gmail.com>
2024-04-17 14:11:35 +0900

    schema修正

--- api/src/db/migrate/20240409064527_change_column_to_user.rb
+++ api/src/db/migrate/20240409064527_change_column_to_user.rb
@@ -1,11 +1,3 @@
 class ChangeColumnToUser < ActiveRecord::Migration[7.0]
-	# 変更内容
-  def up
-    change_column :users, :id, :string
-  end
-
-  # 変更前の状態
-  def down
-    change_column :users, :id, :integer
-  end
+	
 end
