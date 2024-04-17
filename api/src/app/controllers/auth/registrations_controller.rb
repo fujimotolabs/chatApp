c8c43b1f114bcc00c28dfef775f015af467ad02a
ei-sugimoto <eikun1014@gmail.com>
2024-04-17 19:29:48 +0900

    messageとusersのtokenがあればアクセスできるように

--- api/src/app/controllers/auth/registrations_controller.rb
+++ api/src/app/controllers/auth/registrations_controller.rb
@@ -1,5 +1,16 @@
 class Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+    after_action :set_token_info, only: [:create]
+
     def registrations_params
         params.permit(:email, :password, :password_confirmation, :name)
-      end
+    end
+
+    def set_token_info
+        return unless @resource.persisted?
+
+        token = @resource.create_new_auth_token
+        response.set_header('access-token', token['access-token'])
+        response.set_header('client', token['client'])
+        response.set_header('uid', token['uid'])
+    end
 end
