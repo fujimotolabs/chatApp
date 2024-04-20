75408462f76934446866096d4e47cbee653ae8e8
ei-sugimoto <eikun1014@gmail.com>
2024-04-21 01:15:19 +0900

    front実装

--- front/chat/app/(auth)/chatSpace/page.tsx
+++ front/chat/app/(auth)/chatSpace/page.tsx
@@ -2,6 +2,18 @@ import { TextArea } from "@/component/chat/TextArea";
 import { Box } from "@mui/material";
 
 export default function Home() {
+  const cable = ActionCable.createConsumer("ws://localhost:3001/cable");
+  const chatChannel = cable.subscriptions.create("ChatChannel", {
+    connected: () => {
+      console.log("connected");
+    },
+    disconnected: () => {
+      console.log("disconnected");
+    },
+    received: (data) => {
+      console.log(data);
+    },
+  });
   return (
     <div className="relative w-full h-full">
       <Box className="w-full h-ful text-white"></Box>
